d9b9f0de81e231c73e5021cafef759de150e9d8c
최희재 <esc5411@naver.com>
2019-08-12 02:21:51 +0000

    Add Geometric.java

---
+++ src/sequence/Geometric.java
@@ -0,0 +1,17 @@
+package sequence;
+
+public class Geometric {
+    public static void main(String[] args){
+    int R = 3;
+    int A = 2;
+    int S = A;
+    int N = 2;
+    while(true) {
+        A = A * R;
+        S = S + A;
+        N = N + 1;
+        if( N > 100 ) break;
+    }
+    System.out.println(S);
+    }
+}
