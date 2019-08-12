74ce1e41739f597ef2b33c7557be84f4c66022a7
최희재 <esc5411@naver.com>
2019-08-12 02:11:26 +0000

    Add geometric.c

---
+++ Sequence/geometric.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int R = 3;
+    int A = 2;
+    int S = A;
+    int N = 2;
+    while(1) {
+        A = A * R;
+        S = S + A;
+        N = N + 1;
+        if( N > 100 ) break;
+    }
+    printf("%d\n", S);
+}
