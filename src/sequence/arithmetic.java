f7685e752c651e141bb813033c115d8e1c7de07e
최희재 <esc5411@naver.com>
2019-08-12 01:23:01 +0000

    modify error

--- src/sequence/arithmetic.java
+++ src/sequence/arithmetic.java
@@ -1,18 +1,19 @@
 package sequence;
 
 public class arithmetic {
-
-   int A = 2;
-   int D = 6;
-   int S = A;
-   int N = 2;
-   int AN = 0;
-   while(true) {
-       AN = A + (N-1)*D;
-       S = S + AN;
-       N = N + 1;
-       if ( N > 200 ) break;
-       
-   }
-   System.out.println(S);
+    public static void main(String[] args) {
+        
+       int A = 2;
+       int D = 6;
+       int S = A;
+       int N = 2;
+       int AN = 0;
+       while(true) {
+           AN = A + (N-1)*D;
+           S = S + AN;
+           N = N + 1;
+           if ( N > 200 ) break;
+       }
+        System.out.println(S);
+   }  
 }
