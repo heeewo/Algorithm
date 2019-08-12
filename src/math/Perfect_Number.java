b61949828b90096562a25ca6a8ad709b7144135c
최희재 <esc5411@naver.com>
2019-08-12 08:48:24 +0000

    Add Perfect_Number.java

---
+++ src/math/Perfect_Number.java
@@ -0,0 +1,20 @@
+package math;
+
+public class Perfect_Number {
+    public static void main(String[] args){
+    int TN = 0;
+    for (int N = 4; N <= 500; N++){
+        int SUM = 0;
+        int K = N/2;
+        for (int J = 1; J <= K; J++){
+            if (N%J==0)
+                SUM += J;
+        }
+        if (N == SUM) {
+            System.out.printf("N = %d\n", N);
+            TN++;
+        }
+    }
+    System.out.println(TN);
+    }
+}
