28e08a445d339f5712b51cd93dc8b8bc639f4021
최희재 <esc5411@naver.com>
2019-08-19 02:47:57 +0000

    Add Selection_sort.java

---
+++ src/sort/Selection_sort.java
@@ -0,0 +1,26 @@
+package sort;
+
+public class Selection_sort {
+    public static void main(String[] args){
+    
+    int E[] = {95, 75, 85, 100, 50};
+    int i  =  0;
+    int Temp;
+
+    do {
+        int j = i + 1;
+        do {
+            if (E[i] > E[j]){
+                Temp = E[i];
+                E[i] = E[j];
+                E[j] = Temp;
+            }
+            j++;
+        }while(j<5);
+        i++;
+    }while (i<4);
+    for (int a = 0; a < 5; a++)
+        System.out.printf("%d\t", E[a]);
+    System.out.println("");
+    }
+}
